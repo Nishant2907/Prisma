2eb02b574a02761605e43c178b232dafc59f8cd7
Nishant Mishra <nishdrummer@gmail.com>
2023-04-07 13:27:57 +0530

    Prisma

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+import jwt_decode from 'jwt-decode'
+import { Heading, Spinner, Card, Text } from '@chakra-ui/react'
+import './App.css'
+import { pascalStrToSpacedWord } from './helper'
+import Form from './Form'
+
+function App() {
+  const [token, setToken] = useState(null)
+  const [schema, setSchema] = useState(null)
+  const [schemaLink, setClaimLink] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api-staging.polygonid.com/v1/orgs/sign-in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'application/json',
+      },
+      body: JSON.stringify({
+        email: import.meta.env.VITE_POLYGON_ID_EMAIL,
+        password: import.meta.env.VITE_POLYGON_ID_PASSWORD,
+      }),
+    })
+      .then((response) => response.json())
+      .then(({ token }) => {
+        setToken(token)
+        const {
+          account: { organization: issuerId },
+        } = jwt_decode(token)
+
+        const tempSchemaLink = `https://api-staging.polygonid.com/v1/issuers/${issuerId}/schemas/${
+          import.meta.env.VITE_POLYGON_ID_SCHEMA_ID
+        }`
+        setClaimLink(`${tempSchemaLink}/offers`)
+        return { token, tempSchemaLink }
+      })
+      .then(({ token, tempSchemaLink }) => {
+        fetch(tempSchemaLink, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => setSchema(data))
+      })
+  }, [])
+
+  const handleResults = (results) => {
+    fetch(schemaLink, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept-Encoding': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        attributes: Object.keys(results).map((k) => {
+          const removeDashes = results[k].indexOf('-') !== 0
+          const val = removeDashes ? results[k].replaceAll('-', '') : results[k]
+          return {
+            attributeKey: k,
+            attributeValue: parseInt(val),
+          }
+        }),
+      }),
+    })
+      .then((response) => response.json())
+      .then(({ id }) => {
+        window.open(
+          `https://platform-test.polygonid.com/claim-link/${id}`,
+          '_newtab'
+        )
+      })
+  }
+
+  return !!schema ? (
+    <>
+      <Text fontSize='3xl' as='b' mb={5}>Prisma</Text>
+      <Card w="xl" p={10} background="rgba(255,255,255,.90)">
+        <Heading mb={5}> Claim your AGE Verification</Heading>
+        {/* <Heading mb={5}> Claim your {pascalStrToSpacedWord(schema.schema)}</Heading> */}
+        <Form fieldInfo={schema?.attributes} passBackResults={handleResults} />
+      </Card>
+      
+    </>
+  ) : (
+    <Spinner size="xl" />
+  )
+}
+
+export default App
